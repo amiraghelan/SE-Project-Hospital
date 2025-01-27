ff9e814303a0f1b33a3b43ea695312fd0ba737e3
Ali Khadempour <ali.khadempour@outlook.com>
2025-01-27 09:37:16 +0330

    adding time_rate for estimate duration

--- src/models/treatment.py
+++ src/models/treatment.py
@@ -16,22 +16,24 @@ class RandomTreatmentType:
 
 
 class Treatment(BaseEntity):
-    def __init__(self, patient_id: int, doctor_id: int, type: TreatmentType) -> None:
+    def __init__(self, patient_id: int, doctor_id: int, type: TreatmentType, time_rate: int) -> None:
         super().__init__()
         self.patient_id = patient_id
         self.doctor_id = doctor_id
         self.treatment_type = type
         self.start_date = datetime.now()
-        self.duration = self.estimate_duration()
+        self.duration = self.__estimate_duration(time_rate)
         self.end_date = self.start_date + self.duration
 
-    def estimate_duration(self) -> timedelta:
+    def __estimate_duration(self, time_rate) -> timedelta:
+        # 1 day : time_rate * seconds
+        # So, for example 8 days equal to (8 * time_rate) seconds
         treatment_durations = {
-            TreatmentType.FRACTURE_TREATMENT: timedelta(seconds=random.randint(6, 8)),
-            TreatmentType.WOUND_CARE: timedelta(seconds=random.randint(1, 3)),
-            TreatmentType.PHYSIOTHERAPY: timedelta(seconds=random.randint(2, 4)),
-            TreatmentType.BURN_TREATMENT: timedelta(seconds=random.randint(1, 4)),
-            TreatmentType.DISLOCATION_TREATMENT: timedelta(seconds=random.randint(3, 5)),
+            TreatmentType.FRACTURE_TREATMENT: timedelta(seconds=random.randint(6, 8) * time_rate),
+            TreatmentType.WOUND_CARE: timedelta(seconds=random.randint(1, 3) * time_rate),
+            TreatmentType.PHYSIOTHERAPY: timedelta(seconds=random.randint(2, 4) * time_rate),
+            TreatmentType.BURN_TREATMENT: timedelta(seconds=random.randint(1, 4) * time_rate),
+            TreatmentType.DISLOCATION_TREATMENT: timedelta(seconds=random.randint(3, 5) * time_rate),
         }
 
         return treatment_durations.get(self.treatment_type, timedelta(seconds=1))
