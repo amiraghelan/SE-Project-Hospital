9eb21af3e3647fdc61140ed045f8c93db2e66887
amiraghelan <amirhaghelan@gmail.com>
2025-01-31 20:52:43 +0330

    remove commnets

--- src/main.py
+++ src/main.py
@@ -2,67 +2,47 @@ from src.models.hospital import Hospital
 import threading
 import time
 from src.utils.logger import get_logger
+import src.config as config
 
 
 logger = get_logger(__name__)
 
-global_stop_event = threading.Event()
 
 
-def take_snapshot(hospital: Hospital, stop_event: threading.Event):
-    while not stop_event.is_set():
+def take_snapshot(hospital: Hospital):
+    while True:
         hospital.take_snapshot()
-        time.sleep(4)
+        time.sleep(config.SNAPSHOT_CLOCK_INTERVAL / hospital.time_rate)
 
-def admit_patient(hospital: Hospital, stop_event: threading.Event):
-    while not stop_event.is_set():
+def admit_patient(hospital: Hospital):
+    while True:
         hospital.admit_patient()
-        time.sleep(2)
-
-
-# def admit_patient(admit_url: str, discharge_url: str, hospital: Hospital):
-#     while True:
-#         if hospital.get_entity_id():
-#             try:
-#                 hospital.admit_patient(admit_url, discharge_url)
-#                 print(f"In {__name__}.{admit_patient.__name__}: admit_patient called")
-#                 print(100 * "-")
-#             except Exception as error:
-#                 print(f"In {__name__}.{admit_patient.__name__}:\n" + f"Error: {error}")
-#                 print(100 * "-")
-#         time.sleep(10)
+        time.sleep(config.ADMIT_PATIENT_CLOCK_INTERVAL / hospital.time_rate)
 
 
 if __name__ == "__main__":
     logger.info("application started")
-    url = "http://localhost:8000/api"
-    hospital = Hospital("hospital", 15, url)
-
-    # while not register(url, hospital):
-    #     print(f"In main.{__name__}: Trying again in 5 seconds")
-    #     print(100 * "-")
-    #     time.sleep(5)
-    #     register(url, hospital)
+    hospital = Hospital("hospital", 15, config.WORLDMODEL_BASE_URL)
 
     try:
         hospital.register()
         
         snapshot_thread = threading.Thread(
-            target=take_snapshot, args=(hospital, global_stop_event)
+            target=take_snapshot, args=(hospital,)
         )
+        snapshot_thread.daemon = True
         snapshot_thread.start()
         
         admit_thread = threading.Thread(
-            target=admit_patient, args=(hospital, global_stop_event)
+            target=admit_patient, args=(hospital,)
         )
+        admit_thread.daemon = True
         admit_thread.start()
-        # threading.Thread(
-        #     target=admit_patient,
-        #     args=(url + f"/accept-person", url + f"/service-done", hospital),
-        # ).start()
+        
+        while True:
+            pass
 
     except KeyboardInterrupt:
-        global_stop_event.set()
         print(f"In {__name__}: Program interrupted by user. Shutting down...")
     except Exception as error:
         print(f"In {__name__}:\n" + f"Unexpected error: {error}")
