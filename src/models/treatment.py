a9dba58b6ab534bff1ad31a1eb241e60ba4dbf24
amiraghelan <amirhaghelan@gmail.com>
2025-01-31 20:52:50 +0330

    fix treatment

--- src/models/treatment.py
+++ src/models/treatment.py
@@ -3,6 +3,11 @@ from datetime import datetime, timedelta
 
 from src.models.base_model import BaseEntity
 from src.models.enums import TreatmentType
+import src.config as config
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
 
 
 class RandomTreatmentType:
@@ -16,36 +21,38 @@ class RandomTreatmentType:
 
 
 class Treatment(BaseEntity):
-    def __init__(self, patient_id: int, doctor_id: int, type: TreatmentType, time_rate: int) -> None:
+    def __init__(self, patient_id: int, doctor_id: int, treatment_type: TreatmentType, time_rate: int) -> None:
         super().__init__()
         self.patient_id = patient_id
         self.doctor_id = doctor_id
-        self.treatment_type = type
+        self.treatment_type = treatment_type
         self.start_date = datetime.now()
         self.duration = self.__estimate_duration(time_rate)
-        # we should move this to config
-        self.death_rate = 0.2
-        self.is_dead, self.death_time = self.__death_during_treatment()
-        if self.death_time:
-            self.end_date = self.death_time
-        else:
-            self.end_date = self.start_date + self.duration
+        self.is_dead, self.death_offset_seconds = self.__death_during_treatment()
+        if self.is_dead and self.death_offset_seconds:
+            self.duration = self.death_offset_seconds
+        self.end_date = self.start_date + timedelta(seconds=self.duration)
+        
+        logger.info(f"treatmeant created: patient_id={self.patient_id} - duration = {self.duration} - healthy = {not self.is_dead}")
 
-    def __estimate_duration(self, time_rate: int) -> timedelta:
-        treatment_durations = {
-            TreatmentType.FRACTURE_TREATMENT: timedelta(seconds=random.randint(6, 8) * time_rate),
-            TreatmentType.WOUND_CARE: timedelta(seconds=random.randint(1, 3) * time_rate),
-            TreatmentType.PHYSIOTHERAPY: timedelta(seconds=random.randint(2, 4) * time_rate),
-            TreatmentType.BURN_TREATMENT: timedelta(seconds=random.randint(1, 4) * time_rate),
-            TreatmentType.DISLOCATION_TREATMENT: timedelta(seconds=random.randint(3, 5) * time_rate),
-        }
-        return treatment_durations.get(self.treatment_type, timedelta(seconds=1))
+    def __estimate_duration(self, time_rate: int) -> int:
+        match self.treatment_type:
+            case TreatmentType.FRACTURE_TREATMENT:
+                return round(random.randint(6, 8) / time_rate)
+            case TreatmentType.PHYSIOTHERAPY:
+                return round(random.randint(2, 4) / time_rate)
+            case TreatmentType.BURN_TREATMENT:
+                return round(random.randint(1, 4) / time_rate)
+            case TreatmentType.DISLOCATION_TREATMENT:
+                return round(random.randint(3, 5) / time_rate)
+            case _:    
+                # added to prevent linter warning 
+                return 1
 
-    def __death_during_treatment(self) -> (bool, datetime):  # type: ignore
-        if random.random() < self.death_rate:
-            death_offset_seconds = random.randint(0, self.duration.total_seconds())
-            death_time = self.start_date + timedelta(seconds=death_offset_seconds)
-            return True, death_time
+    def __death_during_treatment(self) -> (bool, int):  # type: ignore
+        if random.random() < config.DEATH_RATE:
+            death_offset_seconds : int = random.randint(0, self.duration)
+            return True, death_offset_seconds
         return False, None
 
     def __str__(self) -> str:
@@ -62,5 +69,5 @@ class Treatment(BaseEntity):
             f"{death_info}"
             f"Start Date: {self.start_date.strftime('%Y-%m-%d %H:%M:%S')}\n"
             f"End Date: {self.end_date.strftime('%Y-%m-%d %H:%M:%S')}\n"
-            f"Duration: {self.duration.seconds} seconds\n"
+            f"Duration: {self.duration} seconds\n"
         )
